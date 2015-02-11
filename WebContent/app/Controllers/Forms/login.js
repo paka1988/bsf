599af8339a3867b7f65c9c5ceb69144ecd628967
pk762 <p.kalashnikov@epages.com>
2015-02-11 18:19:05 +0100

    Polymer and jQuery were added.[BSF-0]

---
+++ WebContent/app/Controllers/Forms/login.js
@@ -0,0 +1,28 @@
+/**
+ * This is controller for login form.
+ */
+var login = {}
+
+/**
+ * This function builds login.
+ */
+login.build = function() {
+	
+	return true;
+}
+
+/**
+ * This function hides login form.
+ */
+login.hide = function() {
+	
+	return true;
+}
+
+/**
+ * This function shows login form
+ */
+login.show = function() {
+
+	return true;
+}
